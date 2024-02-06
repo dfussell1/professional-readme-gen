3543a07fcd63369d3a944fbf70057f982eb54bae
dfussell1 <daltonfussell1@gmail.com>
2024-02-06 11:36:56 -0600

    moved files around, added array of questions

---
+++ index.js
@@ -0,0 +1,63 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please add a short description of your application'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What dependencies need to be installed?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What will this application be used for?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines for the application?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'If any, how do tests need to be initiated?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select the license used for your application, if one.',
+        choices: ['MIT', 'Apache 2.0', 'GPL v3', 'Mozilla', 'BSD-2', 'BSD-3', 'Boost', 'Creative Commons Zero', 'Eclipse', 'The Unlicense', 'None']
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
